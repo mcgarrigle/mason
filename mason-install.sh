e7f664c34f077e56171978f0b3675b49c1abd291
root <root@node.foo.local>
2018-03-05 20:51:13 +0000

    next iteration

--- mason-install.sh
+++ mason-install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 yum install -y epel-release
-yum install -y vim git wget bind-utils yum dig
+yum install -y vim git wget bind-utils yum tree
 yum install -y dnsmasq syslinux httpd
+
+mkdir -p /var/lib/tftpboot/images/centos/x86_64/7.4
 
 cp /usr/share/syslinux/pxelinux.0 /var/lib/tftpboot
 cp /usr/share/syslinux/menu.c32   /var/lib/tftpboot
@@ -11,10 +13,11 @@ cp /usr/share/syslinux/mboot.c32  /var/lib/tftpboot
 cp /usr/share/syslinux/chain.c32  /var/lib/tftpboot
 
 mount -o loop centos-gold-7.4.1708.iso /mnt/
-mkdir -p /var/lib/tftpboot/images/centos/x86_64/7.4
 
 cp /mnt/images/pxeboot/* /var/lib/tftpboot/images/centos/x86_64/7.4
 cp -r /mnt /var/www/html/centos-7.4
+
+restorecon -r /var/lib/tftpboot /var/www/html
 
 # -------------------------------------------------------
 
