f6f7977798cfc24061719968f7a46534f0fe4eca
Peter McGarrigle <mcgarrigle@github.com>
2018-03-26 06:48:20 +0000

    Provisoner is now a class instead of a module

--- lib/provisioner.rb
+++ lib/provisioner.rb
@@ -12,45 +12,46 @@ require 'whiskers'
 
 class Provisioner
 
-  def self.mac_address(mac)
+  def initialize(node)
+    @node = node.dup
+    @fqdn = @node["fqdn"]
+    @node["mason.address"] = Mason.config.address
+    @node["mason.url"]     = Mason.config.url
+  end
+
+  def mac_address(mac)
     mac.scan(/../).join(":")
   end
 
-  def self.kickstart(node)
-    node["mason.address"] = Mason.config.address
-    node["mason.url"]     = Mason.config.url
-    fqdn = node["fqdn"]
-    node["interfaces"].each do |interface|
-      interface["mac"] = mac_address(interface["mac"])
-    end
-    template = File.read(Mason.config.kickstart_template)
-    Whiskers.template(template, node)
-  end
-
-  def self.mac_boot(mac)
+  def mac_boot(mac)
     mac = "01" + mac
     mac.downcase.scan(/../).join("-")
   end
 
+  def kickstart
+    @node["interfaces"].each do |interface|
+      interface["mac"] = mac_address(interface["mac"])
+    end
+    template = File.read(Mason.config.kickstart_template)
+    Whiskers.template(template, @node)
+  end
+
   # target = install or localdisk
 
-  def self.bootfile(node, boot = "install")
-    node["mason.boot"]    = boot
-    node["mason.address"] = Mason.config.address
-    node["mason.url"]     = Mason.config.url
-    mac  = mac_boot(node["interfaces"][0]["mac"])
+  def bootfile(boot = "install")
+    @node["mason.boot"] = boot
+    mac  = mac_boot(@node["interfaces"][0]["mac"])
     temp = File.read(Mason.config.boot_template)
-    text = Whiskers.template(temp, node)
+    text = Whiskers.template(temp, @node)
     path = File.join(Mason.config.tftp_dir, mac)
     File.write(path, text)
   end
 
-  def self.dns(node)
-    fqdn = node["fqdn"]
+  def dns
     hosts = Hosts::File.read("/etc/hosts")
-    hosts.elements.delete_if {|h| h.name == fqdn }
-    node["interfaces"].each do |interface|
-      element = Hosts::Entry.new(interface["ip"], fqdn)
+    hosts.elements.delete_if {|h| h.name == @fqdn }
+    @node["interfaces"].each do |interface|
+      element = Hosts::Entry.new(interface["ip"], @fqdn)
       hosts.elements << element
     end
     hosts.write
